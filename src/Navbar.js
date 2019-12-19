34a5b83d083de8da84e51e26703b7be7c25a84f7
Shayan Shaikh <shayanshaikh@Shayans-MacBook-Pro-2.local>
2019-12-18 16:07:46 -0800

    init

---
+++ src/Navbar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { MDBContainer, MDBBtn, MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBIcon } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+class Navbar extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          collapse: false,
+      };
+      this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+        collapse: !this.state.collapse,
+      });
+  }
+
+  render() {
+    const bgPink = {backgroundColor: '#505050'}
+    return(
+      <div>
+        <Router>
+          <header>
+            <MDBNavbar style={bgPink} dark expand="md" scrolling fixed="top">
+              <MDBNavbarBrand href="/">
+                  <strong>SCRUM</strong>
+              </MDBNavbarBrand>
+              <MDBNavbarToggler onClick={ this.onClick } />
+              <MDBCollapse isOpen = { this.state.collapse } navbar>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    {
+                      this.props.user
+                        ? <MDBNavLink to="#" style={{margin:5}}>Hello, {this.props.user.displayName}</MDBNavLink>
+                        : <MDBNavLink to="#"></MDBNavLink>
+                    }             
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    {
+                      this.props.user
+                        ? <MDBBtn size="md" onClick={this.props.signOut}>Sign out</MDBBtn>
+                        : <MDBBtn size="md" onClick={this.props.signInWithGoogle}>Log in</MDBBtn>
+                    }
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </header>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
