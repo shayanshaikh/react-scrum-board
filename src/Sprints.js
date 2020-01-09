d9d888857f1283dbbedf2317367dfbee76668745
Shayan Shaikh <shayanshaikh@Shayans-MacBook-Pro-2.local>
2020-01-08 21:35:51 -0800

    added goal to sprints

--- src/Sprints.js
+++ src/Sprints.js
@@ -14,21 +14,34 @@ class Sprints extends React.Component {
     this.state = {
       modal: false,
       sprints: [],
+      goals: [],
+      selectedGoal: '',
       selectedSprint: ''
     }
   }
     
   toggle = () => {
     this.setState({
-      modal: !this.state.modal
+      modal: !this.state.modal,
+      selectedGoal: ''
+    });
+  }
+
+  selectGoal = (goalName) => {
+    this.setState({
+      selectedGoal: goalName
     });
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
+    if (!this.state.selectedGoal.length) {
+      return;
+    }
     const newItem = {
       releaseID: this.props.releaseID,
-      projectID: this.props.projectID
+      projectID: this.props.projectID,
+      goal: this.state.selectedGoal
     };
 
     const sprintsRef = firebaseApp.database().ref('sprints');
@@ -46,11 +59,27 @@ class Sprints extends React.Component {
         newState.push({
           id: sprint,
           releaseID: sprintsList[sprint].releaseID,
-          projectID: sprintsList[sprint].projectID
+          projectID: sprintsList[sprint].projectID,
+          goal: sprintsList[sprint].goal
         });
       }
       this.setState({
         sprints: newState
+      });
+    });
+    const goalsRef = firebaseApp.database().ref('goals');
+    goalsRef.on('value', (snapshot) => {
+      let goalList = snapshot.val();
+      let newState = [];
+      for (let goal in goalList) {
+        newState.push({
+          id: goal,
+          goalName: goalList[goal].goalName,
+          projectID: goalList[goal].projectID
+        });
+      }
+      this.setState({
+        goals: newState
       });
     });
   }
@@ -82,16 +111,23 @@ class Sprints extends React.Component {
           id: s.id,
           releaseID: s.releaseID,
           projectID: s.projectID,
+          goal: s.goal,
           number: i
         };
         sprints.push(newItem);
         i = i + 1;
       }
     });
+    var goals = [];
+    this.state.goals.forEach ((g) => {
+      if (this.props.projectID === g.projectID) {
+        goals.push(g);
+      }
+    });
 
     return (
       <React.Fragment>
-          <h4 className="w-50 text-center"><MDBBtn color="primary" size="sm" id="projectbtn" onClick={this.handleSubmit}>+New Sprint Plan</MDBBtn></h4>
+          <h4 className="w-50 text-center"><MDBBtn color="primary" size="sm" id="projectbtn" onClick={this.toggle}>+New Sprint Plan</MDBBtn></h4>
           { sprints.length === 0 ? <h3 className="emptyTitle">Looks like you have no sprint plans try creating a new one.</h3> : null }
           {sprints.map(sprint => ( 
             <React.Fragment key={sprint.id} >     
@@ -100,6 +136,7 @@ class Sprints extends React.Component {
             <MDBCardBody>
             <MDBCardTitle>Sprint {sprint.number} <MDBBtn className="deleteTask" color="danger" size="sm" onClick={() => { if (window.confirm("Are you sure you want to delete this permantly?")) this.removeSprint(sprint.id)} }>×</MDBBtn></MDBCardTitle>
             <MDBCardText>
+              Goal: {sprint.goal} <br/>
               {this.state.selectedSprint === sprint.id ? <MDBBtn color="warning" size="sm" onClick={this.closeSprint}>Close</MDBBtn> : <MDBBtn color="info" size="sm" onClick={() => this.selectSprint(sprint.id)}>Open</MDBBtn> }
             </MDBCardText>
             </MDBCardBody>
@@ -108,6 +145,23 @@ class Sprints extends React.Component {
             {this.state.selectedSprint === sprint.id ? <Stories sprintID={this.state.selectedSprint} /> : null}
             </React.Fragment>
           ))}
+          <MDBModal isOpen={this.state.modal} toggle={this.toggle} centered>
+            <MDBModalHeader toggle={this.toggle}>Create a New Sprint</MDBModalHeader>
+            <MDBModalBody>
+              Here is where you will pick the goal you want to guide this sprint.
+              {goals.map(goal => (
+                <MDBCard key={goal.id} className="card">
+                <MDBCardBody>
+                <MDBCardText>{goal.goalName} <br/> { goal.goalName === this.state.selectedGoal ? <MDBBtn color="success" size="sm"><i className="fas fa-check"></i>Selected</MDBBtn> : <MDBBtn color="primary" size="sm" onClick={() => this.selectGoal(goal.goalName) }>Select</MDBBtn> }</MDBCardText>
+                </MDBCardBody>
+                </MDBCard>
+              ))}
+            </MDBModalBody>
+            <MDBModalFooter>
+              <MDBBtn color="secondary" onClick={this.toggle}>Close</MDBBtn>
+              <MDBBtn color="primary" onClick={this.handleSubmit}>Save changes</MDBBtn>
+            </MDBModalFooter>
+          </MDBModal>
       </React.Fragment>
     );
   }
